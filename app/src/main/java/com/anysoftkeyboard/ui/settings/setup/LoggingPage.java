8c57f1d2bef745c5dceb098e618ba03aebbe076d
chenghungma <mzh878@live.com>
2019-07-25 18:20:14 -0700

    Add a login page and the jumping logic and exiting

--- app/src/main/java/com/anysoftkeyboard/ui/settings/setup/LoggingPage.java
+++ app/src/main/java/com/anysoftkeyboard/ui/settings/setup/LoggingPage.java
@@ -23,14 +23,26 @@ public class LoggingPage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging_page);
 
-//        final Button button = (Button) findViewById(R.id.Continue);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//
-//                Intent toLogging = new Intent(LoggingPage.this, LauncherSettingsActivity.class);
-//                startActivity(toLogging);
-//            }
-//        });
+        final Button button = (Button) findViewById(R.id.Continue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toLogging = new Intent(LoggingPage.this, LauncherSettingsActivity.class);
+                startActivity(toLogging);
+            }
+        });
+
+        final Button exit_button = (Button) findViewById(R.id.Exit);
+        exit_button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
 
     }
 }
