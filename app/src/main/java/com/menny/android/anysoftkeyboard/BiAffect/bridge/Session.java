05f72bcb82e60b69ebfa8521fce6c25e043f60e3
Jingran Wang <hikaritenchi@gmail.com>
2019-09-15 19:55:22 -0400

    Update Session JSON object to match upload format, as well as methods to populate those fields

--- app/src/main/java/com/menny/android/anysoftkeyboard/BiAffect/bridge/Session.java
+++ app/src/main/java/com/menny/android/anysoftkeyboard/BiAffect/bridge/Session.java
@@ -1,50 +1,103 @@
 package com.menny.android.anysoftkeyboard.BiAffect.bridge;
 
+import com.menny.android.anysoftkeyboard.BiAffect.Database.Models.AccelerometerData;
+import com.menny.android.anysoftkeyboard.BiAffect.Database.Models.KeyTypeData;
+import com.menny.android.anysoftkeyboard.BiAffect.Database.Models.TouchTypeData;
+
 import org.joda.time.DateTime;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+
+/**
+ * JSON object that represents the correct data format that Bridge Research Manager expects for BiAffect.
+ */
 public class Session {
-    private double duration;
-    private DateTime timestamp;
-    private List<Keylogs> keylogs;
+    private double              duration;
+    private DateTime            timestamp;
+    private List<Keylogs>       keylogs;
     private List<Accelerations> accelerations;
 
-    public Session() {
-        duration = 2.3;
-        timestamp = DateTime.now();
-
-        keylogs = new ArrayList<>();
-        keylogs.add(new Keylogs());
-
-        accelerations = new ArrayList<>();
-        accelerations.add(new Accelerations());
+    /**
+     * Basic constructor for this class that initializes the required time-related parameters
+     *
+     * @param start epoch representing the start of the Session
+     * @param end epoch representing the end of the Session
+     */
+    public Session( long start, long end ) {
+        duration = end - start;
+        timestamp = new DateTime( start );
     }
 
-    private static class Keylogs {
-        private DateTime timestamp;
-        private String value;
-        private double duration;
-        private double distanceFromCenter;
+    /**
+     * Adds the correct Accelerations data from the AccelerometerData from the DB
+     *
+     * @param accelerometerData List of AccelerometerData associated with this Session
+     */
+    public void addAccelerometerData( List<AccelerometerData> accelerometerData ) {
+        accelerations = Observable.fromIterable( accelerometerData )
+                                  .map( Accelerations::new )
+                                  .toList()
+                                  .blockingGet();
+    }
 
-        private Keylogs() {
-            timestamp = DateTime.now();
-            value = "test";
-            duration = 0.83;
-            distanceFromCenter = 3.2;
+    /**
+     * Adds the correct Keylogs data from the KeyTypeData from the DB
+     *
+     * @param keyTypeData List of KeyTypeData associated with this Session
+     */
+    public void addKeyTypeData( List<KeyTypeData> keyTypeData ) {
+        keylogs = Observable.fromIterable( keyTypeData )
+                            .map( Keylogs::new )
+                            .toList()
+                            .blockingGet();
+    }
+
+    /**
+     * Adds the correct Keylogs data from the TouchTypeData from the DB
+     *
+     * @param touchTypeData List of TouchTypeData associated with this Session
+     */
+    public void addTouchTypeData( List<TouchTypeData> touchTypeData ) {
+        keylogs = Observable.merge( Observable.fromIterable( keylogs ),
+                                    Observable.fromIterable( touchTypeData )
+                                              .map( Keylogs::new ) )
+                            .toList()
+                            .blockingGet();
+    }
+
+    /**
+     * Class that maps to Keylogs in the scheme, but contains both KeyTypeData and TouchTypeData
+     */
+    private static class Keylogs {
+        private KeyTypeData   keyTypeData;
+        private TouchTypeData touchTypeData;
+
+        private Keylogs( KeyTypeData keyTypeData ) {
+            this.keyTypeData = keyTypeData;
+        }
+
+        private Keylogs( TouchTypeData touchTypeData ) {
+            this.touchTypeData = touchTypeData;
         }
     }
 
+    /**
+     * Class that maps to Accelerations in the scheme
+     */
     private static class Accelerations {
+        private long   time;
         private double x;
         private double y;
         private double z;
 
-        private Accelerations() {
-            x = 0.33289;
-            y = -0.9381;
-            z = 0.319742;
+        private Accelerations( AccelerometerData source ) {
+            time = source.time;
+            x = source.Acc_X;
+            y = source.Acc_Y;
+            z = source.Acc_Z;
         }
     }
+
 }
