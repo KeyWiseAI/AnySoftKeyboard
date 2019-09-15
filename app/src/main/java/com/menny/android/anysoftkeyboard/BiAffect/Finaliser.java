1c764cf21140b92e452ae74b5bfc75b313277d6d
Jingran Wang <hikaritenchi@gmail.com>
2019-09-15 19:57:18 -0400

    Updates Finalizer to queue Session data upload when the session ends

--- app/src/main/java/com/menny/android/anysoftkeyboard/BiAffect/Finaliser.java
+++ app/src/main/java/com/menny/android/anysoftkeyboard/BiAffect/Finaliser.java
@@ -2,9 +2,19 @@ package com.menny.android.anysoftkeyboard.BiAffect;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.menny.android.anysoftkeyboard.BiAffect.Database.BiADatabaseManager;
-import com.menny.android.anysoftkeyboard.BiAffect.Database.BiAffect_Database;
-import com.menny.android.anysoftkeyboard.BiAffect.Database.Models.SessionData;
+import com.menny.android.anysoftkeyboard.BiAffect.Database.Models.DeviceData;
+import com.menny.android.anysoftkeyboard.BiAffect.bridge.Session;
+
+import org.joda.time.DateTime;
+import org.sagebionetworks.bridge.android.manager.BridgeManagerProvider;
+import org.sagebionetworks.bridge.data.Archive;
+import org.sagebionetworks.bridge.data.JsonArchiveFile;
+
+import java.util.Locale;
+
+import rx.Subscription;
 
 public class Finaliser implements Runnable {
 
@@ -13,6 +23,8 @@ public class Finaliser implements Runnable {
     BiAManager sharedInstance;
     int count=0;
     BiADatabaseManager mBiADatabaseManager;
+
+    private Subscription uploadSubscription;
 
     public Finaliser(long startTime, long endTime, BiADatabaseManager databaseManager){
         super();
@@ -67,6 +79,8 @@ public class Finaliser implements Runnable {
             }
         mBiADatabaseManager.updateSessionData(startTime,endTime);
 
+            uploadSubscription = upload();
+
         }catch (InterruptedException e){
 
         }finally {
@@ -80,4 +94,59 @@ public class Finaliser implements Runnable {
 
         }
     }
+
+    /**
+     * Creates a Session object that represents this session that just got finalized.
+     * Queues the upload of the Session object.
+     * Takes care of unsubscribing correctly from the upload queue.
+     *
+     * @return Subscription that needs to be referenced by uploadSubscription in order to support unsubscribing
+     */
+    private Subscription upload() {
+        Session session = new Session( startTime, endTime );
+
+        try { //we want to wait for 100ms because that's the delay for AccelerometerDataWorker logic.
+            Thread.sleep( 100 );
+        } catch( InterruptedException ignore ) {}
+
+        session.addAccelerometerData( mBiADatabaseManager.getAccelerometerData( startTime, endTime ) );
+
+        session.addKeyTypeData( mBiADatabaseManager.getKeyTypeData( startTime, endTime ) );
+        session.addTouchTypeData( mBiADatabaseManager.getTouchTypeData( startTime, endTime ) );
+
+        Archive.Builder builder = Archive.Builder.forActivity( "KeyboardSession" );
+        builder.addDataFile( new JsonArchiveFile( "Session.json",
+                                                  new DateTime( endTime ),
+                                                  new Gson().toJson( session ) ) );
+
+        DeviceData deviceData = mBiADatabaseManager.getDeviceData();
+        String appVersionString = String.format( Locale.ENGLISH,
+                                                 "%s %s %s dp %s, dpi %s, w %s, h %s",
+                                                 deviceData.manufacturer,
+                                                 deviceData.phoneModel,
+                                                 deviceData.androidVersion,
+                                                 deviceData.pixelDensityLogical,
+                                                 deviceData.pixelDensityDpi,
+                                                 deviceData.deviceWidthPixel,
+                                                 deviceData.deviceHeightPixel );
+        builder.withAppVersionName( appVersionString );
+        builder.withPhoneInfo( BridgeManagerProvider.getInstance().getBridgeConfig().getDeviceName() );
+
+        return BridgeManagerProvider.getInstance()
+                                    .getUploadManager()
+                                    .queueUpload( String.valueOf( startTime ), builder.build() )
+                                    .subscribe(
+                                        success -> {
+                                            Log.d( "Bridge SDK Upload", "upload queued successfully" );
+                                            if( null != uploadSubscription ) {
+                                                uploadSubscription.unsubscribe();
+                                            }
+                                        }, error -> {
+                                            error.printStackTrace();
+                                            if( null != uploadSubscription ) {
+                                                uploadSubscription.unsubscribe();
+                                            }
+                                        } );
+    }
+
 }
